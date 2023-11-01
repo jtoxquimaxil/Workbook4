3368d3bfdcdd262f4310e98c01455139d2184198
jtoxquimaxil <jtoxquimaxil@gmail.com>
2023-11-01 11:51:56 -0400

    completed loop exercise 1

---
+++ scripts/ObjectScripts/product_code.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function parsePartCode(supplierCode) {
+    console.log("Supplier: " + part1.supplierCode +
+    " Product Number: " + part1.productNumber +
+    " Size: " + part1.size);
+}
+let supplierCode = {
+    supplierCode1: "someValue",
+    productNumber: "someValue",
+    size: "someValue",
+}
+
+parsePartCode(supplierCode);
--- scripts/examples.js
+++ scripts/examples.js
@@ -1,45 +1,90 @@
+"use strict";
+
 function printEmployeeAndPay(emp) {
     console.log("Name: " + emp.name);
     console.log("Pay: " + emp.payRate);
-   }
-   let emp1 = {
-    employeeId: "1", 
-    name: "Ezra", 
-    jobTitle: "Theater Teacher", 
+}
+let emp1 = {
+    employeeId: "1",
+    name: "Ezra",
+    jobTitle: "Theater Teacher",
     payRate: 38.46
-    };
-   let emp2 = {
-    employeeId: "2", 
-    name: "Elisha", 
-    jobTitle: "Game Programmer", 
+};
+let emp2 = {
+    employeeId: "2",
+    name: "Elisha",
+    jobTitle: "Game Programmer",
     payRate: 43.27
-    };
-   printEmployeeAndPay(emp1);
-   printEmployeeAndPay(emp2);
+};
+printEmployeeAndPay(emp1);
+printEmployeeAndPay(emp2);
 
 ////////////////////////////////////////////
 
 function createPayStub(id, name, payRate, hoursWorked) {
     let grossPay = 0;
     if (hoursWorked <= 40) {
-    grossPay = payRate * hoursWorked;
+        grossPay = payRate * hoursWorked;
     }
     else {
-    grossPay = (40 * payRate) +
-    ((hoursWorked - 40) * 1.5 * payRate);
+        grossPay = (40 * payRate) +
+            ((hoursWorked - 40) * 1.5 * payRate);
     }
     let payStub = {
-    employeeId: id, 
-    name: name, 
-    grossPay: grossPay
+        employeeId: id,
+        name: name,
+        grossPay: grossPay
     };
     return payStub;
-   }
-   let emp1PayStub = 
+}
+let emp1PayStub =
     createPayStub("1", "Ezra", 38.46, 49);
-   console.log(emp1PayStub.name + " earned $" + 
+console.log(emp1PayStub.name + " earned $" +
     emp1PayStub.grossPay.toFixed(2));
-   let emp2PayStub = 
+let emp2PayStub =
     createPayStub("2", "Elisha", 43.27, 42);
-   console.log(emp2PayStub.name + " earned $" + 
+console.log(emp2PayStub.name + " earned $" +
     emp2PayStub.grossPay.toFixed(2));
+
+////////////////////////////////////
+
+// While LOOPS//
+
+// let num = 1;
+// let i = 1;
+// while (i < 5) {
+//     num = num * 2;
+//     console.log(num);
+//     i++;
+// }
+
+/////////////////////////////////////
+
+/// do/while LOOPS////
+
+// let num = 1;
+// let i = 1;
+// do {
+//  num = num * 2;
+//  console.log(num);
+//  i++;
+// } while (i < 5)
+
+//////// for LOOPS///
+
+// let num = 1;
+// let i;
+// for (i = 0; i < 5; i++) {
+//  num = num * 2;
+//  console.log(num);
+// }
+
+//// Breaking Out of a LOOP///
+
+let num = 1;
+let i = 1;
+while (i < 100) {
+ num = num * 2;
+ if (num >= 100) break;
+ i++;
+}
